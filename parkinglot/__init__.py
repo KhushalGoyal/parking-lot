13ada8ba2cb5f512d396ebf28bae09c767ee9fed
Khushal Goyal <khushal@vedbiz.com>
2018-12-10 15:30:59 +0530

    Changes For Python3 and greater

--- parkinglot/__init__.py
+++ parkinglot/__init__.py
@@ -3,7 +3,7 @@ import pandas as pd
 def creating_parking_lot(totalCars):
     totalCarsList = [1+x for x in range(totalCars)]
     #print(totalCarsList)
-    print "Created a parking lot with" ,totalCars,"slots \n"
+    print("Created a parking lot with" ,totalCars,"slots \n")
     df = pd.DataFrame(index=range(0, totalCars), columns=['RegistrationSlotNo','CarNo','Colour'], dtype='float')
     df['RegistrationSlotNo']=totalCarsList
     return  df
--- parkinglot/test2.py
+++ parkinglot/test2.py
@@ -4,10 +4,10 @@ def main():
     stack = []
     totalCars = int(input("For how many Cars you want to create a parking space \n"))
     carslot = parkinglot.creating_parking_lot(totalCars)
-    print carslot.to_string(index=False)," \n",
+    print(carslot.to_string(index=False)," \n")
     count = 1
     while True:
-        print "\n"
+        print("\n")
         value = int(input(
             "To park your vehicle press 1 \n"
             "To leave from the parking lot press 2 \n"
@@ -15,13 +15,13 @@ def main():
             "To access the database press 4 \n"
             "To Exit press 5 \n"))
         if value == 1:
-            platenumber, colour = (raw_input("To park your Vehicle enter your Car Plate number and Colour \n")).split(" ")
+            platenumber, colour = (input("To park your Vehicle enter your Car Plate number and Colour \n")).split(" ")
             if len(stack)==0:
                 carslot.loc[carslot['RegistrationSlotNo'] == count, 'CarNo'] = platenumber
                 carslot.loc[carslot['RegistrationSlotNo'] == count, 'Colour'] = colour
                 #print count, "^^^^^^^^^"
                 if count > totalCars:
-                    print "Sorry No More Space \n"
+                    print("Sorry No More Space \n")
                     break
                 count += 1
             else:
@@ -29,41 +29,41 @@ def main():
                 carslot.loc[carslot['RegistrationSlotNo'] == val, 'CarNo'] = platenumber
                 carslot.loc[carslot['RegistrationSlotNo'] == val, 'Colour'] = colour
                 if count > totalCars:
-                    print "Sorry No More Space \n"
+                    print("Sorry No More Space \n")
             #print carslot.to_string(index=False)," \n"
         elif value == 2:
             #print carslot.to_string(index=False)," \n"
-            slot = int(raw_input("Enter your Registration Slot number :\n"))
+            slot = int(input("Enter your Registration Slot number :\n"))
             stack.append(slot)
             carslot.loc[carslot['RegistrationSlotNo'] == slot, 'CarNo'] = "Na"
             carslot.loc[carslot['RegistrationSlotNo'] == slot, 'Colour'] = "Na"
             #print carslot, "\n^^^^^^^^^"
             #print stack
         elif value == 3:
-            print carslot.to_string(index=False)," \n"
+            print(carslot.to_string(index=False)," \n")
         elif value == 4:
-            print carslot.to_string(index=False),"\n"
+            print(carslot.to_string(index=False),"\n")
             while True:
                 data = int(input("To get Registration Slot Number using colour press 1 \n"
                                  "To get Car Number using colour press 2 \n"
                                 "To get Car Number with Registration number press 3 \n"))
                 if data == 1:
-                    col = raw_input("Enter your car colour :\n")
+                    col = input("Enter your car colour :\n")
                     pdata = carslot[carslot.Colour == col]
-                    print "Your Registration Slot Number : ",pdata.RegistrationSlotNo.to_string(index=False), "\n"
+                    print("Your Registration Slot Number : ",pdata.RegistrationSlotNo.to_string(index=False), "\n")
                     break
                 elif data == 2:
-                    col = raw_input("Enter your car colour :\n")
+                    col = input("Enter your car colour :\n")
                     pdata1 = carslot[carslot.Colour == col]
-                    print "Your Car Number : ",pdata1.CarNo.to_string(index=False), "\n", "\n"
+                    print("Your Car Number : ",pdata1.CarNo.to_string(index=False), "\n", "\n")
                     break
                 elif data == 3:
                     try:
-                        regnum = raw_input("Enter your registration number :\n")
+                        regnum = input("Enter your registration number :\n")
                         padata2 = carslot[carslot.RegistrationSlotNo == regnum]
                         if padata2.RegistrationSlotNo == regnum:
-                            print "Your Car Number : ",padata2.CarNo.to_string(index=False), "\n"
+                            print("Your Car Number : ",padata2.CarNo.to_string(index=False), "\n")
                     except ValueError:
-                        print "Not Fount", "\n"
+                        print("Not Fount", "\n")
         elif value == 5:
             break
